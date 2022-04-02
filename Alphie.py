0f2fdaf7bdf7542d7bdeaf502c888f761b8fe617
Dominic Wüst <dominic.wuest@hotmail.com>
2022-04-02 20:00:08 +0200

    Made Alphie respond to people greeting him

--- Alphie.py
+++ Alphie.py
@@ -2,16 +2,34 @@ import discord
 from discord.ext import commands
 
 import os
+import random
+import re
 
 bot = commands.Bot(command_prefix=':) ')
+
+emojis = {
+    "alph" : "<:alph:959849626020761680>"
+}
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the Pikmin bloom"))
     print('Alphie is ready to pluck!')
 
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    # Respond to messages similar to "Hello Alphie!"
+    if re.match("^hello alph(ie)?!?$", message.content, re.I):
+        responses = ["Hello!", "Who said that?", "Wow, you're huge!", "You're not from Koppai, are you?", "While you're here, can you help me carry this Sunseed Berry?", "Wow, you must be able to throw so many Pikmin at once!"]
+        await message.channel.send(random.choice(responses) + " " + str(emojis["alph"]))
+
+    await bot.process_commands(message)
+
 @bot.command()
 async def ping(ctx):
-    await ctx.send(f'Pong! `{round(bot.latency * 1000)} ms`')
+    await ctx.send(f'Pong! `{round(bot.latency * 1000)}ms`')
 
 bot.run(os.getenv('API_TOKEN'))
