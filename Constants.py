59d45ec10a621bfa1c6196d0e2260667b86d9a00
Dominic Wüst <dominic.wuest@hotmail.com>
2022-04-06 10:50:10 +0200

    Implemented Constants initialization

--- Constants.py
+++ Constants.py
@@ -1,10 +1,26 @@
 import os
 
-emojis = {
-    "alph" : "<:alph:959849626020761680>"
-}
+# The ID of the home guild 
+__HOME_GUILD_ID = 0
+# The actual object of the home guild
+__HOME_GUILD = None
 
+# A dictionary of emojis from the home guild
+emojis = {}
+
+# The ID's of users who are allowed to use privileged commands
 authorized_ids = []
-# AUTHORIZED_IDS should be in the form of ID_0,ID_1,...,ID_n
-for id in os.getenv('AUTHORIZED_IDS').split(','):
-    authorized_ids.append(int(id))
+
+# Actually initializes the constants
+async def initialize_constants(bot):
+    # Read the ID of the home guild and get it's object
+    __HOME_GUILD_ID = int(os.getenv('HOME_GUILD'))
+    __HOME_GUILD = await bot.fetch_guild(__HOME_GUILD_ID)
+
+    # Add all emojis of the home guild
+    for emoji in __HOME_GUILD.emojis:
+        emojis[emoji.name] = str(emoji)
+
+    # AUTHORIZED_IDS should be in the form of ID_0,ID_1,...,ID_n
+    for id in os.getenv('AUTHORIZED_IDS').split(','):
+        authorized_ids.append(int(id))
