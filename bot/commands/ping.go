4c46b451dc728a5939a74eaf073d27923c9c17b0
Dominic Wüst <dominic.wuest@hotmail.com>
2022-04-16 17:13:58 +0200

    Rewrote the bot in go

---
+++ bot/commands/ping.go
@@ -0,0 +1,29 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/DominicWuest/Alphie/constants"
+
+	discord "github.com/bwmarrin/discordgo"
+)
+
+type Ping struct{}
+
+// Reply with Pong! and the latency of the bot in ms
+func (s *Ping) HandleCommand(bot *discord.Session, ctx *discord.MessageCreate, args []string) {
+	if len(args) == 1 {
+		message := fmt.Sprintf("Pong! `%dms` %s", int(bot.HeartbeatLatency().Milliseconds()), constants.Emojis["alph"])
+		bot.ChannelMessageSend(ctx.ChannelID, message)
+	} else {
+		bot.ChannelMessageSend(ctx.ChannelID, s.Help())
+	}
+}
+
+func (s Ping) Desc() string {
+	return "Shows the bots latency."
+}
+
+func (s Ping) Help() string {
+	return "The command does not take any additional arguments."
+}
