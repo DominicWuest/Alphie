09a6fbce155b30eb22b5bbe7004ab42e4eec7965
Dominic Wüst <dominic.wuest@hotmail.com>
2022-04-20 08:47:46 +0200

    Implemented connecting to DB and creating users

--- bot/commands/todo.go
+++ bot/commands/todo.go
@@ -1,12 +1,20 @@
 package commands
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/DominicWuest/Alphie/constants"
 
 	discord "github.com/bwmarrin/discordgo"
+	_ "github.com/lib/pq"
 )
 
-type Todo struct{}
+type Todo struct {
+	psqlConn string
+}
 
 func (s *Todo) HandleCommand(bot *discord.Session, ctx *discord.MessageCreate, args []string) {
 	if len(args) == 1 {
@@ -41,29 +49,84 @@ func (s Todo) Help() string {
 }
 
 func (s Todo) Init(args ...interface{}) constants.Command {
+	s.psqlConn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOSTNAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_USER"),
+	)
+
+	// Check if DB connection works
+	db, err := sql.Open("postgres", s.psqlConn)
+	if err != nil {
+		fmt.Println("Error connecting to the database: ", err)
+	}
+
+	defer db.Close()
+
+	success := false
+	err = nil
+	// Try to ping 10 times, maybe we need to wait for the DB to boot up first
+	for i := 0; i < 10; i++ {
+		if err = db.Ping(); err == nil {
+			success = true
+			err = nil
+			break
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	if !success {
+		fmt.Println("Error connecting to the database:", err)
+	}
+
 	return &s
 }
 
 func (s *Todo) add(bot *discord.Session, ctx *discord.MessageCreate, args []string) {
+	s.checkUserPresence(ctx.Author.ID)
 	bot.ChannelMessageSend(ctx.ChannelID, "todo.add")
 }
 
 func (s *Todo) list(bot *discord.Session, ctx *discord.MessageCreate, args []string) {
+	s.checkUserPresence(ctx.Author.ID)
 	bot.ChannelMessageSend(ctx.ChannelID, "todo.list")
 }
 
 func (s *Todo) done(bot *discord.Session, ctx *discord.MessageCreate, args []string) {
+	s.checkUserPresence(ctx.Author.ID)
 	bot.ChannelMessageSend(ctx.ChannelID, "todo.done")
 }
 
 func (s *Todo) remove(bot *discord.Session, ctx *discord.MessageCreate, args []string) {
+	s.checkUserPresence(ctx.Author.ID)
 	bot.ChannelMessageSend(ctx.ChannelID, "todo.remove")
 }
 
 func (s *Todo) subscribe(bot *discord.Session, ctx *discord.MessageCreate, args []string) {
+	s.checkUserPresence(ctx.Author.ID)
 	bot.ChannelMessageSend(ctx.ChannelID, "todo.subscribe")
 }
 
 func (s *Todo) archive(bot *discord.Session, ctx *discord.MessageCreate, args []string) {
+	s.checkUserPresence(ctx.Author.ID)
 	bot.ChannelMessageSend(ctx.ChannelID, "todo.archive")
+}
+
+// Checks if a user is present in the database and inserts them if not
+func (s *Todo) checkUserPresence(id string) {
+	db, _ := sql.Open("postgres", s.psqlConn)
+	defer db.Close()
+	rows, _ := db.Query(
+		`SELECT id FROM todo.discord_user WHERE id=$1`,
+		id,
+	)
+	defer rows.Close()
+	if !rows.Next() { // User not yet in DB
+		db.Exec(
+			`INSERT INTO todo.discord_user(id) VALUES ($1)`,
+			id,
+		)
+	}
 }
--- bot/go.mod
+++ bot/go.mod
@@ -2,7 +2,10 @@ module github.com/DominicWuest/Alphie
 
 go 1.18
 
-require github.com/bwmarrin/discordgo v0.24.0
+require (
+	github.com/bwmarrin/discordgo v0.24.0
+	github.com/lib/pq v1.10.5
+)
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
