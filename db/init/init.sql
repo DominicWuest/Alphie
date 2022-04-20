3eb62ed27558fddc5b3d30060356ffb321b99934
Dominic Wüst <dominic.wuest@hotmail.com>
2022-04-20 08:47:46 +0200

    Implemented connecting to DB and creating users

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,20 +1,20 @@
 CREATE SCHEMA todo;
 
 CREATE TABLE todo.discord_user (
-    id SERIAL,
+    id VARCHAR(19), -- discord snowflake ID's
     PRIMARY KEY(id)
 );
 
 CREATE TABLE todo.task (
   id  SERIAL NOT NULL,
-  creator SERIAL references todo.discord_user (id) NOT NULL,
+  creator VARCHAR(19) references todo.discord_user (id) NOT NULL,
   title TEXT NOT NULL,
   description TEXT,
   PRIMARY KEY(id)
 );
 
 CREATE TABLE todo.completed (
-    discord_user SERIAL references todo.discord_user (id) NOT NULL,
+    discord_user VARCHAR(19) references todo.discord_user (id) NOT NULL,
     task SERIAL references todo.task (id) NOT NULL,
     PRIMARY KEY (discord_user, task)
 );
