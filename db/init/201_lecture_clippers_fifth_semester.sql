ebc297471494570cff9c256f21e6a0ba8a4fd1f1
Dominic Wüst <dominic.wuest@hotmail.com>
2022-07-27 12:11:42 +0200

    Added database schema for lecture clippers and their schedules

---
+++ db/init/201_lecture_clippers_fifth_semester.sql
@@ -0,0 +1,7 @@
+-- Computer Systems
+INSERT INTO lecture_clippers.clippers VALUES
+    ('252-0217-00', 'H');
+
+INSERT INTO lecture_clippers.schedule VALUES
+    ('252-0217-00', 'cab-g-61', '0 10 * * MON', 120),
+    ('252-0217-00', 'cab-g-61', '0 10 * * FRI', 120);
