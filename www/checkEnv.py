70dac2d50d16b483b869ddec31c1766663a30d76
Dominic Wüst <dominic.wuest@hotmail.com>
2022-09-10 14:44:18 +0200

    Added environment variable checking for the web-server

---
+++ www/checkEnv.py
@@ -0,0 +1,17 @@
+import os
+
+needed_vars = ['STUDENT_AUTH_PATH', 'CDN_DOMAIN']
+
+
+def error(var):
+    raise Exception(var + ' is not defined, aborted')
+
+
+list(map(lambda x: error(x) if not os.getenv(x) else None, needed_vars))
+
+if os.getenv('STUDENT_AUTH_ENABLED'):
+    # Ensure JWT public key is set if auth is enabled
+    if not os.getenv('JWT_PUBLIC_KEY'):
+        error('JWT_PUBLIC_KEY')
+
+print('Environment variable checking succeeded. Proceeding...')
--- www/src/routes/clip/[id]/+page.server.ts
+++ www/src/routes/clip/[id]/+page.server.ts
@@ -1,7 +1,5 @@
 import type { PageServerLoadEvent } from './$types';
 
-// TODO: Verify environment variables are set at startup
-
 export function load({ params }: PageServerLoadEvent): { id: string; domain: string } {
 	const domain = process.env.CDN_DOMAIN || '';
 	return {
