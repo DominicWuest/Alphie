7762cca0f2d4a8bb645b4a88caf9f86515785ad4
Dominic Wüst <dominic.wuest@hotmail.com>
2022-09-13 18:56:46 +0200

    Worked on the frontend and fixed static files 404ing after building

--- www/src/routes/clip/[id]/+page.server.ts
+++ www/src/routes/clip/[id]/+page.server.ts
@@ -1,9 +1,15 @@
 import type { PageServerLoadEvent } from './$types';
 
-export function load({ params }: PageServerLoadEvent): { id: string; domain: string } {
+export function load({ params }: PageServerLoadEvent): {
+	id: string;
+	domain: string;
+	mail: string;
+} {
 	const domain = process.env.CDN_DOMAIN || '';
+	const mail = process.env.DEV_MAIL_ADDR || '';
 	return {
 		id: params.id,
-		domain
+		domain,
+		mail
 	};
 }
